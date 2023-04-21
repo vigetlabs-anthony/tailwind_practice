86c23e5d545fd3b5de8205c5313a71165749227c
Anthony <anthony.ngo@viget.com>
2023-04-20 21:29:54 -0700

    add clsx and complete hot button

--- tailwind-conditional-classes/src/App.jsx
+++ tailwind-conditional-classes/src/App.jsx
@@ -1,33 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { clsx } from 'clsx';
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount((count) => count + 1)
+  }
+
+  const handleReset = () => {
+    setCount(0)
+  }
 
   return (
     <>
+  <div className={clsx('flex justify-center items-center h-screen', 
+       {
+         'bg-yellow-100': count > 2,
+         'bg-yellow-200': count > 4,
+         'bg-orange-300': count > 8,
+         'bg-orange-400': count > 12,
+         'bg-red-400': count > 16,
+         'bg-violet-500': count > 20,
+         "bg-[url(./assets/image.webp)] bg-cover": count > 25
+       }
+     )}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Vite + React + Tailwind</h1>
+      {count < 30 && (
+        <div className='flex justify-center'>
+          <button onClick={handleClick}>
+            count is {count}
+          </button>
+        </div>
+      )}
+      {count === 30 && (
+        <div className='flex justify-center'>
+        <button onClick={handleReset}>
+          Reset
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      )}
+      </div>
+    </div>
     </>
   )
 }
